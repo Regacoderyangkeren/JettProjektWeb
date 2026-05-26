b07287f9388f534394410f78ca592eec3c123ed1
LivingTree <aryarega2811@gmail.com>
2026-05-26 07:24:22 +0700

    Probe Firestore reads in health endpoint

--- app/Http/Controllers/Api/HealthController.php
+++ app/Http/Controllers/Api/HealthController.php
@@ -5,11 +5,28 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Services\Firebase\FirebaseService;
 use Illuminate\Http\JsonResponse;
+use Throwable;
 
 class HealthController extends Controller
 {
     public function firebase(FirebaseService $firebase): JsonResponse
     {
+        $firestoreRead = [
+            'ok' => false,
+            'errorType' => null,
+        ];
+
+        try {
+            foreach ($firebase->firestore()->collection($firebase->collectionName('users'))->limit(1)->documents() as $document) {
+                break;
+            }
+
+            $firestoreRead['ok'] = true;
+        } catch (Throwable $exception) {
+            report($exception);
+            $firestoreRead['errorType'] = class_basename($exception);
+        }
+
         return response()->json([
             'ok' => true,
             'firebase' => [
@@ -17,6 +34,7 @@ class HealthController extends Controller
                 'database' => $firebase->firestoreDatabase(),
                 'credentialsConfigured' => $firebase->credentialsConfigured(),
                 'transport' => $firebase->firestoreTransport(),
+                'firestoreRead' => $firestoreRead,
             ],
             'collections' => config('jettprojekt.collections'),
         ]);
